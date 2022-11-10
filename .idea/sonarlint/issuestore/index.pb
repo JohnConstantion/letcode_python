c2122bd04074ced74796c39aff5a5abcf89019f4
chenxianjun_ext <chenxianjun_ext@cop.cmsoft.com.cn>
2022-11-10 16:05:02 +0800

    学习print的想关信息

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+7
+main.py,f/1/f1bdda93d9a278e358509d498e17d97764c1fb29
+E
+algorithm/__init__.py,1/a/1a08efd084c9ca86d1775222e1e990cae6175d5c
--- learn/hello.py
+++ learn/hello.py
@@ -1,4 +1,7 @@
 # coding: utf-8
 import os
 
-print(os.getcwd())
+# print函数加end可以到达不换行的操作，默认是进行换行的。在一个print李可以执行打印多个条件
+print(os.getcwd(), end='')
+print('今天早点休息')
+print('1', '2', '3')
